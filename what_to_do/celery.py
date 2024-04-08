bc39a267062f33974dd08d67d1435569f41bf450
Marcin <marcin.sak.of@gmail.com>
2024-04-08 19:06:17 +0200

    Add sending of notifications on given hour (19.05)

--- what_to_do/celery.py
+++ what_to_do/celery.py
@@ -2,12 +2,25 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from django.conf import settings
+from datetime import  timedelta
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'what_to_do.settings')
 app = Celery('what_to_do')
-app.config_from_object('django.conf:settings')
+app.conf.timezone = 'Europe/Warsaw'
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+CELERY_BEAT_SCHEDULE = {
+    'check_user_activity': {
+        'task': 'what_to_do_app.tasks.check_user_activity',
+        'schedule': crontab(hour='19', minute='05')
+    }
+}
+
+app.conf.beat_schedule = CELERY_BEAT_SCHEDULE
+
 
 @app.task(bind=True)
 def debug_task(self):
