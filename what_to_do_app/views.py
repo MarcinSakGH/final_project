a662074693d63f04d63ab40d05dfd81c9279309e
Marcin <marcin.sak.of@gmail.com>
2024-04-22 16:03:18 +0200

    Add success message after succesful creation of new user

--- what_to_do_app/views.py
+++ what_to_do_app/views.py
@@ -9,6 +9,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.db.models import Prefetch
 from django.utils import timezone
 from django.contrib.auth import login
+from django.contrib import messages
 from django.template.loader import get_template
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, TemplateView, UpdateView, View
@@ -44,6 +45,11 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'User created successfully.')
+        return response
 
 
 class CustomLoginView(LoginView):
